904876b53932d6b630e81b7b2bd14c7a26eca2f6
Benedict Notoane <benni@MacBookPro.lan>
2025-08-19 00:28:27 +0200

    syntax fixes

--- src/data/clients.js
+++ src/data/clients.js
@@ -7,7 +7,7 @@ const clients = [
     spotify: "https://open.spotify.com/artist/5IllKMkLRxFoaA33dGTq5J",
     latestSong: "https://open.spotify.com/embed/artist/5IllKMkLRxFoaA33dGTq5J", // Spotify embed
     instagramEmbed: "https://www.instagram.com/p/DJRbUSgKaE-/embed", // Insta embed
-    bio: "Artist description goes here... Artist description goes here...Artist description goes here... Artist description goes here... Artist description goes here...Artist description goes here... Artist description goes here... Artist description goes here...Artist description goes here... Artist description goes here... Artist description goes here...Artist description goes here... Artist description goes here... Artist description goes here...Artist description goes here... Artist description goes here... Artist description goes here...Artist description goes here... Artist description goes here... Artist description goes here...Artist description goes here... Artist description goes here... Artist description goes here...Artist description goes here...",
+    bio: "Artist description goes here...",
   },
   // Later, just add more clients here...
 ];
