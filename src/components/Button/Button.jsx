5ccb12ba5e2854e1181fa4ba80fb2a80c08c4778
Benedict Notoane <benni@MacBookPro.lan>
2025-09-10 00:01:24 +0200

    fix button import bug

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,6 +1,7 @@
 import styles from "./Button.module.css";
 
 const Button = ({ children, classname, onClick }) => {
+
   return (
     <button className={`${classname} ${styles.button}`} onClick={onClick}>
       {children}
